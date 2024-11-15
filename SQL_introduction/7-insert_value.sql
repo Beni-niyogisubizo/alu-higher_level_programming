bde8cb1a663f47fac3adaa846f6bd3c0c7527fdd
Beni-niyogisubizo <b.niyogisub@alustudent.com>
2024-11-15 20:58:36 +0100

    Task 7 correction

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,6 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Script to insert a new row into the table `first_table` in the specified database
+-- New row: id = 89, name = 'Best School'
+
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
+

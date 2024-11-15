03b6a10df5ff3899c9155fb4c8a0282d618fe604
Beni-niyogisubizo <b.niyogisub@alustudent.com>
2024-11-15 21:58:26 +0100

    Task 0 correction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,4 @@
--- Task: List all privileges of the MySQL users user_0d_1 and user_0d_2
--- This script checks for the existence of the users and lists their privileges.
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
--- Check if user_0d_1 exists
-SELECT
-    'user_0d_1' AS user,
-    CASE
-	        WHEN COUNT(*) = 0 THEN 'Users don’t exist'
-			        ELSE GROUP_CONCAT(DISTINCT PRIVILEGE_TYPE) 
-					    END AS privileges
-					FROM
-					    information_schema.USER_PRIVILEGES
-					WHERE
-					    users='localhost'
-					;
-
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;

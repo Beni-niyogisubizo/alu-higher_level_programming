94b3202c67c61ddfc3b114c9ae22c69f35a3964d
Beni-niyogisubizo <b.niyogisub@alustudent.com>
2024-11-28 00:00:04 +0100

    Correction for Tast 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Fetches http://0.0.0.0:5050/status using the requests package."""
+"""Fetches a URL and displays the response body in a specific format."""
 
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("http://0.0.0.0:5050/status")
+    url = "http://0.0.0.0:5050/status"
+    response = requests.get(url)
     content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))

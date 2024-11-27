519bef282373fc97c8b7e44b8464ba1ba96f452b
Beni-niyogisubizo <b.niyogisub@alustudent.com>
2024-11-27 23:52:41 +0100

    Correction for Tast 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using the requests package."""
+"""Fetches http://0.0.0.0:5050/status using the requests package."""
 
 import requests
 
-if __name__ == '__main__':
-    response = requests.get('https://alu-intranet.hbtn.io/status')
+if __name__ == "__main__":
+    response = requests.get("http://0.0.0.0:5050/status")
     content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))
